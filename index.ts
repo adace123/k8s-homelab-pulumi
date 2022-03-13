5dd4619599515e2c3f83f2273bed15866aa33b8b
aaron.feigenbaum <aaron@enervee.com>
2022-03-12 17:28:33 -0800

    remove Vault and Traefik from Pulumi for now

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
-import { traefik, vaultServer } from './modules/kubernetes/';
-import { argoCDRootApp } from './modules/kubernetes/argocd';
-import { cluster } from './modules/kubernetes/cluster';
+import * as pulumi from "@pulumi/pulumi";
+
+// import { traefik, vaultServer } from './modules/kubernetes/';
+import { argocd } from "./modules/kubernetes/argocd";
+import { cluster } from "./modules/kubernetes/cluster";
 
 export = {
-  kubeconfig: cluster.kubeConfig,
-  vaultToken: vaultServer.credentials.root_token,
-  argoCD: argoCDRootApp.urn
+  kubeconfig: pulumi.secret(cluster.kubeConfig),
+  // vaultToken: vaultServer.credentials.root_token,
+  argoCD: argocd
 };
--- modules/kubernetes/argocd/index.ts
+++ modules/kubernetes/argocd/index.ts
@@ -1,26 +1,27 @@
-import * as k8s from '@pulumi/kubernetes';
-import { resolve } from 'path';
+import * as k8s from "@pulumi/kubernetes";
+import { resolve } from "path";
 
-import { provider } from '../cluster';
+import { provider } from "../cluster";
 
-const argocd = new k8s.helm.v3.Release(
-  'argocd',
+export const argocd = new k8s.helm.v3.Release(
+  "argocd",
   {
-    name: 'argocd',
-    namespace: 'argocd',
+    name: "argocd",
+    namespace: "argocd",
     repositoryOpts: {
-      repo: 'https://argoproj.github.io/argo-helm'
+      repo: "https://argoproj.github.io/argo-helm"
     },
-    chart: 'argo-cd',
+    version: "3.35.4",
+    chart: "argo-cd",
     createNamespace: true
   },
   { provider }
 );
 
-export const argoCDRootApp = new k8s.yaml.ConfigFile(
-  'argocd-root-app',
+const rootApp = new k8s.yaml.ConfigFile(
+  "argocd-app",
   {
-    file: `${resolve('.')}/apps/argocd/root.yaml`
+    file: `${resolve(".")}/modules/kubernetes/argocd/root.yaml`
   },
-  { provider, dependsOn: [argocd], parent: provider }
+  { provider, dependsOn: [argocd], parent: argocd }
 );
