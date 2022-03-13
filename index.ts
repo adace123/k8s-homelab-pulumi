2b123452e8a948c1d619bb6f73c3e0ad006f935d
aaron.feigenbaum <aaron@enervee.com>
2022-03-12 22:02:16 -0800

    start migrating to Flux + sops

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import * as pulumi from "@pulumi/pulumi";
 
-// import { traefik, vaultServer } from './modules/kubernetes/';
-import { argocd } from "./modules/kubernetes/argocd";
 import { cluster } from "./modules/kubernetes/cluster";
+// import { traefik, vaultServer } from './modules/kubernetes/';
+// import { argocd } from "./modules/kubernetes/argocd";
+import { flux } from "./modules/kubernetes/flux";
 
 export = {
   kubeconfig: pulumi.secret(cluster.kubeConfig),
+  flux
   // vaultToken: vaultServer.credentials.root_token,
-  argoCD: argocd
+  // argoCD: argocd
 };
---
+++ modules/kubernetes/flux/index.ts
@@ -0,0 +1,45 @@
+import * as k8s from "@pulumi/kubernetes";
+import * as pulumi from "@pulumi/pulumi";
+import { resolve } from "path";
+
+import { provider } from "../cluster";
+
+export const flux = new k8s.helm.v3.Release(
+  "flux",
+  {
+    name: "flux",
+    namespace: "flux-system",
+    chart: "flux2",
+    version: "0.14.0",
+    createNamespace: true,
+    repositoryOpts: {
+      repo: "https://fluxcd-community.github.io/helm-charts"
+    }
+  },
+  { provider }
+);
+
+const fluxConfig = new pulumi.Config("flux");
+const ageKey = fluxConfig.requireSecret("agekey");
+
+const ageKeySecret = new k8s.core.v1.Secret(
+  "age-key",
+  {
+    metadata: {
+      name: "sops-age",
+      namespace: "flux-system"
+    },
+    data: {
+      "age.agekey": ageKey.apply((a) => a)
+    }
+  },
+  { provider, dependsOn: [flux], parent: flux }
+);
+
+const fluxRootApp = new k8s.kustomize.Directory(
+  "flux-base",
+  {
+    directory: `${resolve(".")}/cluster/base`
+  },
+  { provider, dependsOn: [ageKeySecret], parent: flux }
+);
